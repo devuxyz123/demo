275a59fd3d8c9e43c9ea53d6d461309be8c2a13d
devuxyz123 <devmishra.98@gmail.com>
2026-06-08 18:03:00 +0530

    local changes hasbeen done

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,10 +4,13 @@ import com.model.UserRecord;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 @SpringBootApplication
@@ -43,9 +46,48 @@ public class DemoApplication {
 
     @DeleteMapping("/delete/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public String deleteProduct(@PathVariable Long id) {
-        return "Product deleted with ID "+10;
+    public  ResponseEntity<String> deleteProduct(@PathVariable Long id) {
+        if (id <= 0||id>=100) {
+            throw new InvalidInputException("Product ID must be greater than zero.");
+        }
+
+
+
+
+//        if(id==0)
+//            return String.valueOf(new NullPointerException("ID can not be zero"));
+
+        return ResponseEntity.ok("Product deleted with ID "+id);
     }
+
+    @GetMapping("product/{id}")
+    public String getOrder(@PathVariable String id) {
+        // Simulating a database lookup failure
+        if ("404".equals(id)) {
+            throw new ProductNotFoundException("ProductNotFoundException",id);
+        }
+
+        if (id.isEmpty()||id.isBlank()) { // Simulating database check
+            throw new ResourceNotFoundException("ID is blank or empty");
+        }
+
+        return "Product details for ID: " + id;
+    }
+
+    // --- LOCAL EXCEPTION HANDLER ---
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleProductNotFound(ProductNotFoundException ex) {
+        Map<String, Object> errorBody = new HashMap<>();
+        errorBody.put("timestamp", System.currentTimeMillis());
+        errorBody.put("status", HttpStatus.NOT_FOUND.value());
+        errorBody.put("error", "Handling locally Product Not Found");
+        errorBody.put("message", ex.getMessage());
+        errorBody.put("requestedId", ex.getOrderId());
+
+        return new ResponseEntity<>(errorBody, HttpStatus.NOT_FOUND);
+    }
+
+
 
     public static void main(String[] args) {
 
