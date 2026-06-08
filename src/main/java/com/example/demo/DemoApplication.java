b4e749e9f15f92a5d25219b94a56710111301c61
devuxyz123 <devmishra.98@gmail.com>
2026-06-08 18:11:18 +0530

    added one line into class DemoApplication

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -92,6 +92,7 @@ public class DemoApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(DemoApplication.class, args);
+        System.out.println("Hello Main Class");
     }
 
 }
